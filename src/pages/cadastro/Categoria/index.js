ad8468a44b2e2598782fac9a4a8cd9575c12a48d
Rodrigo Dias <diasbass@hotmail.com>
2020-07-28 17:13:51 -0300

    Form New Category

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -2,15 +2,31 @@ import React from 'react';
 import PageDefault from '../../../components/components/PageDefault';
 import { Link } from 'react-router-dom';
 
-function CadastroCategoria(){
-    return (
-      <PageDefault>
-          <h1>Cadastro de Categoria</h1>
-          <Link to="/">
-              Voltar para home
-          </Link>
-      </PageDefault>
-    )
-  }
+function CadastroCategoria() {
+  return (
+    <PageDefault>
+      <h1>Cadastro de Categoria</h1>
+
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  )
+}
 
   export default CadastroCategoria;
